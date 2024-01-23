e409b9edd9586170fa44a3a815256317b03fb082
Sergiomms <mariano.srgio@gmail.com>
2024-01-23 11:22:00 -0300

    feat(password): created password tretment and forgot password treatment, with services, controllers and repositories also created createTokenUser migration and token_users entity

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,45 @@
+import { getCustomRepository } from "typeorm";
+import { hash } from "bcryptjs";
+import { isAfter, addHours } from 'date-fns';
+import AppError from "@shared/errors/AppError";
+import UsersRepository from "../typeorm/repositories/UserRepository";
+import UsersTokensRepository from "../typeorm/repositories/UserTokensRepository";
+
+
+interface InterfaceRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: InterfaceRequest): Promise<void> {
+
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UsersTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if(!userToken) {
+      throw new AppError('User Token does not exists.')
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if(!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if(isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.')
+    }
+
+    user.password = await hash(password, 8);
+
+  }
+
+}
+
+export default ResetPasswordService
