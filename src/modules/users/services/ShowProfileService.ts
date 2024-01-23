2c6d01b36777d7b112a269fe4d1d98491929345d
Sergiomms <mariano.srgio@gmail.com>
2024-01-23 17:29:32 -0300

    chore: created the UpdateProfileService so the user can change his name, email or password

--- src/modules/users/services/ShowProfileService.ts
+++ src/modules/users/services/ShowProfileService.ts
@@ -1,19 +1,24 @@
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UserRepository";
+import AppError from "@shared/errors/AppError";
 
 interface IRequest {
   user_id: string;
 }
 
 class ShowProfileService {
-  public async execute({user_id}: IRequest): Promise<User[]> {
+  public async execute({user_id}: IRequest): Promise<User> {
 
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const users = await usersRepository.find()
+    const user = await usersRepository.findById(user_id)
 
-    return users
+    if(!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user
 
   }
 }
