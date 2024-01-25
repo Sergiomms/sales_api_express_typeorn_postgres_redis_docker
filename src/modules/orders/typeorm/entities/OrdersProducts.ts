09959cc7d6687792cf907d946a2ddaa50a266196
Sergiomms <mariano.srgio@gmail.com>
2024-01-25 17:52:50 -0300

    Created createOrderService and showOrderService and added method findAllByIds in the productsRepository

--- src/modules/orders/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -1,6 +1,7 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import Order from "./Order";
 import Product from "@modules/products/typeorm/entities/Product";
+import { CONNREFUSED } from "dns";
 
 @Entity('orders_products')
 class OrdersProducts {
@@ -14,6 +15,12 @@ class OrdersProducts {
   @ManyToOne(() => Product, product => product.order_products)
   @JoinColumn({ name: 'product_id' })
   product: Product;
+
+  @Column()
+  order_id: string;
+
+  @Column()
+  product_id: string;
 
   @Column('decimal')
   price: number;
